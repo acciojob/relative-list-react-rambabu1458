2e9622cc7458d51a7c72c025697fe515228fdd60
rambabu1458 <ramu.y95@gmail.com>
2025-05-01 14:51:34 +0530

    PageLayout

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
-import React from 'react'
-import RelativeList from './RelativeList '
+import React from "react";
+import RelativeList from "./RelativeList ";
+import PageLayout from "./PageLayout";
 
 const App = () => {
+  const date = new Date().getFullYear();
   return (
     <div id="main">
-               Do not remove the main div
-               <RelativeList/>
+      Do not remove the main div
+      <RelativeList />
+      <PageLayout
+        header={<h1>Welcome to my website</h1>}
+        footer={<p>&copy; {date} My Website</p>}
+      >
+        <p>This is the content of my website.</p>
+      </PageLayout>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
